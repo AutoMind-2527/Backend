7bf9df4085181f634f521f2eb5dc7dbebb780226
Andrej <146711514+AndrejNedic@users.noreply.github.com>
2025-10-16 15:12:52 +0200

    Update GPSData.cs

---
+++ AutoMindBackend/Models/GPSData.cs
@@ -0,0 +1,10 @@
+namespace AutoMindBackend.Models;
+
+public class GpsData
+{
+    public int Id { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int VehicleId { get; set; }
+}
