688539876ce15e736ba1b66e419a4d8816b9d454
Andrej <146711514+AndrejNedic@users.noreply.github.com>
2025-10-17 09:21:08 +0200

    imported the JWT stuff redid the program.cs so we can use JWT to login and register

--- AutoMindBackend/Program.cs
+++ AutoMindBackend/Program.cs
@@ -1,6 +1,9 @@
 using AutoMindBackend.Data;
 using AutoMindBackend.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +18,27 @@ builder.Services.AddScoped<VehicleService>();
 builder.Services.AddScoped<TripService>();
 builder.Services.AddScoped<DataSeeder>();
 builder.Services.AddScoped<GpsService>();
+builder.Services.AddScoped<AuthService>();
+
+// JWT Authentication konfigurieren
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
 
 var app = builder.Build();
 
+// --- PIPELINE ---
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,7 +46,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
+
 
 using (var scope = app.Services.CreateScope())
 {
