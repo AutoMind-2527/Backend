789b44836af6cf75a42a17330f2525d4352e21f0
Andrej <146711514+AndrejNedic@users.noreply.github.com>
2026-01-20 16:04:32 +0100

    Update backend-deploy.yml

--- AutoMindBackend/Program.cs
+++ AutoMindBackend/Program.cs
@@ -177,7 +177,7 @@ if (/*app.Environment.IsDevelopment()*/ true)
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("api/swagger/v1/swagger.json", "AutoMind API");
+        c.SwaggerEndpoint("v1/swagger.json", "AutoMind API");
 
         // Keycloak OAuth2 Settings für Swagger-Login
         c.OAuthClientId("automind-backend");
